b781e14da4983865d11bd8baf40df30d1ea7c229
tianyu <tianyu@gamil.com>
2019-02-12 10:59:47 +0800

    init

---
+++ day01/1.ajax练习--服务器端/server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const db = require('./db')
+const Cities = require('./model/cities')
+const app = express()
+const PORT = 3000;
+
+
+;(async()=>{
+  //等待数据库连接完毕
+  await db;
+
+  //获取所有省份信息
+  app.get('/getAllProvince',(req,res)=>{
+    Cities.find({level:1},{province:1,name:1,_id:0},(err,data)=>{
+      if(!err){
+        res.json({state:1,data})
+      }else{
+        console.log(err)
+        res.json({state:0,data:'网络不稳定'})
+      }
+    })
+  })
+
+})()
+
+
+
+
+
+app.listen(PORT,(err)=>{
+  if(!err){
+    console.log(`服务器已经启动了，端口号为${PORT}`)
+  }else{
+    console.log(err);
+  }
+})
